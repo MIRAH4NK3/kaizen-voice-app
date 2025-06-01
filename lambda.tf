da86c56d66d94e265a66f50f808a039073edad3c
MIRAH4NK3 <mira.minkova@gmail.com>
2025-06-01 10:46:07 +0200

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -6,45 +6,61 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_iam_role" "lambda_exec_role" {
   name = "kaizen_lambda_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Action = "sts:AssumeRole",
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
       Principal = {
         Service = "lambda.amazonaws.com"
-      },
-      Effect = "Allow",
-      Sid    = ""
+      }
     }]
   })
 }
 
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "kaizen_lambda_dynamodb_policy"
-  role = aws_iam_role.lambda_exec_role.id
-
+resource "aws_iam_policy" "lambda_policy" {
+  name = "kaizen_lambda_policy"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
-        Action = [
-          "dynamodb:PutItem"
-        ],
-        Resource = aws_dynamodb_table.kaizen_logs.arn
-      },
-      {
-        Effect = "Allow",
-        Action = [
+        Effect   = "Allow",
+        Action   = [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents"
         ],
         Resource = "*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "dynamodb:PutItem"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "s3:PutObject",
+          "s3:GetObject"
+        ],
+        Resource = "arn:aws:s3:::kaizen-voice-raw-dresden/*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "transcribe:StartTranscriptionJob"
+        ],
+        Resource = "*"
       }
     ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
 
 resource "aws_lambda_function" "kaizen_story_handler" {
